0c2e5da799682eb410533192f44c6199bdf75a8a
Diana Popoviciu <dipopovi@cisco.com>
2020-04-13 21:09:27 +0300

    initial commit:

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static int semester = 2;
+	private int instanceVar = 3;
+	public static void main(String[] args) {
+		int var = 2020;
+		System.out.println("Atelierul Digital " + var);
+		System.out.println("First day " + semester);
+	}
+}
